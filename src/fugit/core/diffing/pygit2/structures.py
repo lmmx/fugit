182dc3bc072461ba6b30312e6f030a928228021f
Louis Maddox <louismmx@gmail.com>
2023-12-20 00:00:26 +0000

    perf: Comment out unused fields of pygit2 data models

--- src/fugit/core/diffing/pygit2/structures.py
+++ src/fugit/core/diffing/pygit2/structures.py
@@ -2,33 +2,28 @@ from __future__ import annotations
 
 from typing import Annotated, Literal
 
-from pydantic import (
-    BaseModel,
-    BeforeValidator,
-    Field,
-    computed_field,
-)
+from pydantic import BaseModel, BeforeValidator
 
 __all__ = ("DiffInfoPG2",)
 
 
 class DiffFile(BaseModel):
-    flags: int
-    id: Annotated[str, BeforeValidator(str)]  # hex string coerced from `Oid` class
-    mode: int
+    # flags: int
+    # id: Annotated[str, BeforeValidator(str)]  # hex string coerced from `Oid` class
+    # mode: int
     path: str
-    raw_path: str
-    size: int
+    # raw_path: str
+    # size: int
 
 
 class DiffDelta(BaseModel):
-    flags: int
-    is_binary: bool
+    # flags: int
+    # is_binary: bool
     new_file: DiffFile
-    nfiles: int
+    # nfiles: int
     old_file: DiffFile
-    similarity: int
-    status: int
+    # similarity: int
+    # status: int
     status_char: Annotated[str, BeforeValidator(lambda method: method.__call__())]
 
 
@@ -39,14 +34,14 @@ class DiffLine(BaseModel):
     num_lines: int
     old_lineno: int
     origin: Literal[" ", "-", "+"]  # Alternatively use to discriminate union
-    raw_content: bytes
+    # raw_content: bytes
 
 
 class DiffHunk(BaseModel):
-    new_start: int
-    new_lines: int
-    old_start: int
-    old_lines: int
+    # new_start: int
+    # new_lines: int
+    # old_start: int
+    # old_lines: int
     header: str  # This might be a string that represents the hunk header
     # Including lines might require another model if they have a complex structure
     lines: list[DiffLine]
@@ -55,23 +50,23 @@ class DiffHunk(BaseModel):
 class DiffPatch(BaseModel):
     delta: DiffDelta
     hunks: list[DiffHunk]
-    line_stats: tuple[int, int, int] = Field(repr=False, exclude=True)
     text: str
+    # line_stats: tuple[int, int, int] = Field(repr=False, exclude=True)
 
-    @computed_field
-    @property
-    def context(self) -> int:
-        return self.line_stats[0]
+    # @computed_field
+    # @property
+    # def context(self) -> int:
+    #     return self.line_stats[0]
 
-    @computed_field
-    @property
-    def additions(self) -> int:
-        return self.line_stats[1]
+    # @computed_field
+    # @property
+    # def additions(self) -> int:
+    #     return self.line_stats[1]
 
-    @computed_field
-    @property
-    def deletions(self) -> int:
-        return self.line_stats[2]
+    # @computed_field
+    # @property
+    # def deletions(self) -> int:
+    #     return self.line_stats[2]
 
 
 class DiffInfoPG2(DiffPatch):
