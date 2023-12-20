8e8ad6563408afe8045718bc1fcd42a6da7e754d
Louis Maddox <louismmx@gmail.com>
2023-12-20 16:07:20 +0000

    refactor: use TypeAdapter to filter by change type; slightly optimise highlighter with match statement

--- src/fugit/core/diffing/logic.py
+++ src/fugit/core/diffing/logic.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+from typing import Annotated
+
 from git import Repo
-from pydantic import TypeAdapter, ValidationError
+from line_profiler import profile
+from pydantic import AfterValidator, TypeAdapter, ValidationError
 from pygit2 import Repository
 from rich.text import Text
 
@@ -27,6 +31,7 @@ def load_diff(config: DiffConfig) -> list[str]:
     return load_diff_pygit2(config) if config.pygit2 else load_diff_gitpython(config)
 
 
+@profile
 def process_diff(
     console: FugitConsole,
     diff_info: DiffInfoGP | DiffInfoPG2,
@@ -34,14 +39,56 @@ def process_diff(
     config: DiffConfig,
 ) -> None:
     ...
-    if diff_info.change_type in config.change_type:
-        filtrate = diff_info.text
-        if STORE_DIFFS:
-            diffs.append(filtrate)
-        console.submit(Text(diff_info.overview, style="bold yellow underline"))
-        # This simulates the render process (`Console.render_str`)
-        console.submit(*highlight_diff(filtrate))
-        console.file_count += 1
+    filtrated = diff_info.text
+    if STORE_DIFFS:
+        diffs.append(filtrated)
+    console.submit(Text(diff_info.overview, style="bold yellow underline"))
+    # This simulates the render process (`Console.render_str`)
+    highlighted = (
+        [Text(line) for line in filtrated.splitlines(keepends=True)]
+        if config.plain
+        else highlight_diff(filtrated)
+    )
+    console.submit(*highlighted)
+    console.file_count += 1
+    return
+
+
+highlight_patterns = {
+    "hunk_context": (r"^@@.*", "bold white"),  # applied first (whole line)
+    "hunk_header": (r"^@@.*?@@ ", "blue"),  # applied second (only inside @ signs)
+}
+
+
+@profile
+def highlight_diff(diff: str) -> list[Text]:
+    """This replaces the highlighter applied by `Console.render_markup`."""
+    # TODO express this as a Rich highlighter class
+    diff_lines = []
+    for line in diff.splitlines(keepends=True):
+        match initial_char := line[0]:
+            case "+":
+                diff_lines.append(Text(line, style="green"))
+                continue
+            case "-":
+                diff_lines.append(Text(line, style="red"))
+                continue
+            case _:
+                diff_line = Text(line, style="white")
+                match initial_char:
+                    case "@":
+                        for pattern, style in highlight_patterns.values():
+                            diff_line.highlight_regex(pattern, style=style)
+                diff_lines.append(diff_line)
+    return diff_lines
+
+
+@dataclass
+class ChangeTypeChecker:
+    change_types: list[str]
+
+    def check(self, diff: DiffInfoPG2) -> DiffInfoPG2 | None:
+        return diff if (diff.change_type in self.change_types) else None
 
 
 def load_diff_pygit2(config: DiffConfig) -> list[str]:
@@ -54,8 +101,12 @@ def load_diff_pygit2(config: DiffConfig) -> list[str]:
     repo_diff_patch = repo.diff(tree, cached=True)
     diffs: list[str] = []
     with fugit_console.pager_available() as console:
-        ta = TypeAdapter(list[DiffInfoPG2])
-        for diff_info in ta.validate_python(repo_diff_patch, from_attributes=True):
+        ct_checker = ChangeTypeChecker(change_types=config.change_type)
+        ct_validator = AfterValidator(ct_checker.check)
+        ct_gate = Annotated[DiffInfoPG2, ct_validator]
+        ta = TypeAdapter(list[ct_gate | None])
+        validated_diffs = ta.validate_python(repo_diff_patch, from_attributes=True)
+        for diff_info in filter(None, validated_diffs):
             process_diff(
                 console=console,
                 diff_info=diff_info,
@@ -95,21 +146,3 @@ def load_diff_gitpython(config: DiffConfig) -> list[str]:
             )
             del diff_info
     return diffs
-
-
-def highlight_diff(diff: str) -> list[Text]:
-    """This replaces the highlighter applied by `Console.render_markup`."""
-    # TODO express this as a Rich highlighter class
-    highlight_patterns = {
-        "removed": (r"^\+.*", "green"),
-        "added": (r"^-.*", "red"),
-        "hunk_context": (r"^@@.*", "bold white"),  # applied first (whole line)
-        "hunk_header": (r"^@@.*?@@ ", "blue"),  # applied second (only inside @ signs)
-    }
-    diff_lines = []
-    for line in diff.splitlines(keepends=True):
-        diff_line = Text(line, style="white")
-        for pattern, style in highlight_patterns.values():
-            diff_line.highlight_regex(pattern, style=style)
-        diff_lines.append(diff_line)
-    return diff_lines
